e6740a2b445acd1b3c8cab0c4d8d7e45a830f4fc
Łukasz Broś <lukasz.bros.sp@gmail.com>
2016-02-21 11:45:20 +0100

    start of the fourth day

--- app/controllers/mainctrl.js
+++ app/controllers/mainctrl.js
@@ -2,7 +2,7 @@ angular.module('exampleApp')
 	.controller('mainCtrl', function() {
 
     var vm = this;
-    vm.notes = [{},{},{}];
+    vm.notes = ["0","1","2"];
     vm.countSticks = 0;
     vm.countArticles = 0;
     var targetElementID = null;
@@ -60,12 +60,12 @@ document.addEventListener("drop", handleDrop);
 
     function handleDragEnter(event){
         if ( event.target.className == dropClassName) {
-            event.target.style.border = "3px dotted red";
+            event.target.style.border = "0.5vw dotted red";
         }
     }
 
     function handleDragOver(event){
-        event.preventDefault();   
+        event.preventDefault();  
     }
 
     function handleDragLeave(event){
@@ -78,38 +78,80 @@ document.addEventListener("drop", handleDrop);
 
     function handleDrop(event){
         event.preventDefault();
-        event.target.style.border = "";
         var data = event.dataTransfer.getData("Text"),
             draggedNode = document.getElementById(data),
             targetEvent = event.target; 
+
+        //those elements checks if target element is article or its child
+        var targetSelect,
+            parentId = targetEvent.parentNode.id,
+            parentIdLetters = parentId.match(/[a-z]+/g).join();
+        console.log('Szukam pustego!');
+        switch(parentIdLetters){
+            // jeśli zostanie zaznaczona karteczka
+            case "article":
+                var targetSelect = event.target.parentNode
+                break;
+            // jesli zostanie zaznaczony obszar
+            case "cont":
+                var targetSelect = event.target
+                break;
+        }
+
+        event.target.style.border = "";
+        targetSelect.style.paddingBottom = "30%";
 
         // It checks if a new element is being adding
         if (data === "yellow" || data === "green") {
             console.log("Dodano nowy element");
 
-            // it checks if target area is empty
+            //element to add a new content
+            function addNewStick(articleItem){
+                var nodeCopy = draggedNode.cloneNode(true);
+                nodeCopy.id = "newId" + vm.countSticks;
+                articleItem.appendChild(nodeCopy);
+                var newNoteImg = document.getElementById('newId' + vm.countSticks);
+                newNoteImg.className = "sticks__img";
+                newNoteImg.style.opacity = "1";
+
+                var stickyNoteText = document.createElement("div");
+                var nodeText = document.createTextNode("This is new stick nr: " + vm.countSticks);
+                stickyNoteText.appendChild(nodeText);
+                stickyNoteText.className = "stickOptions";
+
+                articleItem.appendChild(stickyNoteText);
+
+                newNoteImg.parentNode.id = "article" + vm.countArticles;
+
+                vm.countSticks++;
+                vm.countArticles++;
+            }
+
+            // it checks if target area is empty and adds a new stick
             if (targetEvent.id === "" && targetEvent.className !=="stickOptions" && targetEvent.className !== "sticks" && targetEvent.className !== "ng-scope") {
-                    var nodeCopy = draggedNode.cloneNode(true);
-                    nodeCopy.id = "newId" + vm.countSticks; /* We cannot use the same ID */
-                    targetEvent.appendChild(nodeCopy);
-                    var newNoteImg = document.getElementById('newId' + vm.countSticks);
-                    newNoteImg.className = "sticks__img";
-                    newNoteImg.style.opacity = "1";
-
-                    var stickyNoteText = document.createElement("div");
-                    var nodeText = document.createTextNode("This is new stick nr: " + vm.countSticks);
-                    stickyNoteText.appendChild(nodeText);
-                    stickyNoteText.className = "stickOptions";
-
-                    targetEvent.appendChild(stickyNoteText);
-
-                    newNoteImg.parentNode.id = "article" + vm.countArticles;
-
-                    vm.countSticks++;
-                    vm.countArticles++;
+                addNewStick(targetEvent);
             } else {
                 // The infrmation that the target area is not empty
                 console.log("element jest pełny");
+                var getArticle = targetSelect;
+                var containerNode = document.getElementById(targetSelect.parentNode.id);
+                var firstChildNode = containerNode.firstChild;
+                getArticle = firstChildNode;
+                if (getArticle.id !== "") {
+                    while(getArticle.id !== "" && getArticle !== null){
+                        getArticle = getArticle.nextSibling;
+                        if (getArticle === null) {
+                            console.log('Nie ma miejsca');
+                            var newDiv = document.createElement('article');
+                            var newDivClass = newDiv.className = "sticks__article droptarget"
+                            containerNode.appendChild(newDiv);
+                            getArticle = containerNode.lastChild;
+                        };
+                    };
+                }
+
+                //Check if has space to add then add new stick/stick on a new space
+                addNewStick(getArticle);
             }
 
         // Here are events for existing notes manipulation
@@ -121,6 +163,8 @@ document.addEventListener("drop", handleDrop);
             // it checks if the img has been dragged
             if (draggedNode.id.includes('newId')) {
                 console.log('złapano obrazek');
+                console.log('id chwyconego obrazka: ' + draggedNode.id);
+                console.log('id chwyconego artykułu: ' + draggedNode.parentNode.id);
                 var allContent = draggedNode.parentNode.innerHTML;
                 var decectElement = targetEvent.childNodes.length > 1 ? "has children" : "no children";
                 var allContent = draggedNode.parentNode.innerHTML,
@@ -135,7 +179,7 @@ document.addEventListener("drop", handleDrop);
 
                     // the new one
                     event.target.innerHTML = allContent;
-                    event.target.id = "article" + (vm.countArticles - 1);
+                    event.target.id = oldId;
 
                     // the old one
                     console.log("Dragged to no children area");
@@ -147,28 +191,16 @@ document.addEventListener("drop", handleDrop);
                     if (draggedNode.id === targetEvent.id || draggedNode.parentNode.id === targetEvent.parentNode.id){
                         console.log("element is the same!");
                     } else {
-                        var targetSelect;
-                        var parentId = targetEvent.parentNode.id;
-                        var parentIdLetters = parentId.match(/[a-z]+/g).join();
-                        var PrimaryStickID = draggedNode.id;
-                        var PrimaryArticleID = draggedNode.parentNode.id;
-                        console.log('Zastąpiono!');
-                        switch(parentIdLetters){
-                            // jeśli zostanie zaznaczona karteczka
-                            case "article":
-                                var targetSelect = event.target.parentNode
-                                break;
-                            // jesli zostanie zaznaczony obszar
-                            case "cont":
-                                var targetSelect = event.target
-                                break;
-                        }
+
+                        console.log('id docelowego obrazka: ' + targetSelect.id);
+                        console.log('id docelowego artykułu: ' + targetSelect.id);
+                        console.log('parent ID jeśli obszar: ' + event.target.parentNode.id);
 
                         // draggedNode = document.getElementById(data),
 
                         // Changin parent node id of source
                         draggedNode.parentNode.id = targetSelect.id;
-                        targetSelect.id = PrimaryArticleID;
+                        targetSelect.id = oldId;
 
                         // Copy of the second note
                         var copy = targetSelect.innerHTML;
